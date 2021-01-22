555c39f4794d179fab7745b97eb238d75398e097
warren clark <wclarkcito@gmail.com>
2021-01-22 15:54:59 -0800

    created global variables trying different jquery functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,34 @@
+$(document).ready(function () {
 
-//displays current date and time
+    //global variables
 
-var dt = new Date(); var time = dt
-var currentdate = new Date(); var datetime = "Now: " + currentdate;
-$("#currentDay").html(currentdate)
-
-
+    var days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+    var months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 
 
 
 
+    //displays current date and time
+
+    var dt = new Date(); var time = dt
+    var currentdate = new Date(); var datetime = "Now: " + currentdate;
+
+    $("#currentDay").html(currentdate)
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+})
