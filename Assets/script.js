820d152a0fc6ae9223634662836de18f19094e9b
warren clark <wclarkcito@gmail.com>
2021-01-22 13:59:03 -0800

    current date and time now displays at the top of the page

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+
+//displays current date and time
+
+var dt = new Date(); var time = dt
+var currentdate = new Date(); var datetime = "Now: " + currentdate;
+$("#currentDay").html(currentdate)
+
+
+
+
+
+
+
+
+
+
+
